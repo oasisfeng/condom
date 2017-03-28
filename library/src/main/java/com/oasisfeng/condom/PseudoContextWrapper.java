c1b7e2bf9c5346031c626ec585ec28796fc27440
oasisfeng <git@id.oasisfeng.com>
2017-03-28 14:48:27 +0800

    Add missing license header to PackageManagerWrapper.

--- library/src/main/java/com/oasisfeng/condom/PseudoContextWrapper.java
+++ library/src/main/java/com/oasisfeng/condom/PseudoContextWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Oasis Feng. All rights reserved.
+ * Copyright (C) 2017 Oasis Feng. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
